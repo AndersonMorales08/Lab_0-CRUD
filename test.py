da583bdb7224456d6bc9704d594448551c427095
brrodriguezd <brrodriguezd@unal.edu.co>
2024-04-08 21:59:30 -0500

    conexion base de datos

---
+++ test.py
@@ -0,0 +1,26 @@
+from supabase import create_client, Client
+from datetime import date
+
+#inicialización
+url: str = "https://uesgiyebiofvboxlzrpk.supabase.co"
+key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVlc2dpeWViaW9mdmJveGx6cnBrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI2MjUzNzAsImV4cCI6MjAyODIwMTM3MH0.pwdmCjWA-IW2h3YvrtwxF_DC2e_OMB44kR12iBTOLX8"
+supabase: Client = create_client(url, key)
+
+#queries
+response = supabase.table('Municipio').select("*").execute()
+def insertar(num_documento: str, tipo_documento: int, primer_nombre: str, segundo_nombre: str, 
+        primer_apellido: str, segundo_apellido: str, telefono:int, fecha_nacimiento:date, sexo: bool):
+    response = supabase.table('Persona').insert({
+        "num_documento": num_documento, 
+        "tipo_documento": tipo_documento, 
+        "primer_nombre": primer_nombre,
+        "segundo_nombre": segundo_nombre,
+        "primer_apellido": primer_apellido,
+        "segundo_apellido": segundo_apellido,
+        "telefono": telefono,
+        "fecha_nacimiento": str(fecha_nacimiento),
+        "sexo": sexo,
+    }).execute()
+    return response
+
+insertar("100000", 1, "brayan", "camilo", "rodriguez", "diaz", 100000, date(2000,12,20), True)
